3c23a26cf1ad1c452c015d1f74f3a37672596ff7
f83y55 <f83y55@gmail.com>
2023-03-21 22:56:04 +0100

    commit

--- Ex_2_13.py
+++ Ex_2_13.py
@@ -3,47 +3,44 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 # Graphe a priori orienté : on utilise DiGraph à la place de Graph
-G = nx.DiGraph()
+G = nx.Graph()
 
 # On crée les arêtes ; préremplissage possible avec :
-# >>> [print(f"#{k}.\nG.add_edge(\"\", \"\", weight=)") for k in range(1,19)]
+# >>> [print(f"#{k}.\nG.add_edge(\"\", \"\", weight=)") for k in range(1,16)]
+# Ext : extérieur
 
 #1.
-G.add_edge("Départ", "Marché", weight=4)
+G.add_edge("Ext", "I", weight=6)
 #2.
-G.add_edge("Départ", "Forêt", weight=1)
+G.add_edge("Ext", "P1", weight=11)
 #3.
-G.add_edge("Forêt", "Marché", weight=2)
+G.add_edge("P1", "P2", weight=3)
 #4.
-G.add_edge("Forêt", "Capitale", weight=7)
+G.add_edge("I", "P2", weight=5)
 #5.
-G.add_edge("Marché", "Col du Nord", weight=5)
+G.add_edge("P2", "P3", weight=2)
 #6.
-G.add_edge("Marché", "Capitale", weight=3)
+G.add_edge("P3", "Ext", weight=10)
 #7.
-G.add_edge("Col du Nord", "Refuge du devin", weight=3)
+G.add_edge("I", "At", weight=3)
 #8.
-G.add_edge("Capitale", "Refuge du devin", weight=4)
+G.add_edge("I", "P5", weight=4)
 #9.
-G.add_edge("Capitale", "Palais du roi", weight=10)
+G.add_edge("P2", "Ad", weight=4)
 #10.
-G.add_edge("Refuge du devin", "Épée", weight=20)
+G.add_edge("P4", "At", weight=5)
 #11.
-G.add_edge("Refuge du devin", "Grotte du dragon", weight=32)
+G.add_edge("At", "P5", weight=8)
 #12.
-G.add_edge("Refuge du devin", "Palais du roi", weight=5)
+G.add_edge("P5", "Ad", weight=5)
 #13.
-G.add_edge("Palais du roi", "Cartes", weight=6)
+G.add_edge("Ext", "P4", weight=11)
 #14.
-G.add_edge("Cartes", "Trésor", weight=30)
+G.add_edge("Ext", "At", weight=9)
 #15.
-G.add_edge("Cartes", "Épée", weight=7)
+G.add_edge("Ext", "P5", weight=11)
 #16.
-G.add_edge("Épée", "Grotte du dragon", weight=8)
-#17.
-G.add_edge("Épée", "Trésor", weight=18)
-#18.
-G.add_edge("Grotte du dragon", "Trésor", weight=9)
+G.add_edge("Ext", "Ad", weight=4)
 
 
 # On prépare le graphe pour l'affichage avec matplotlib
@@ -54,11 +51,10 @@ nx.draw_networkx_labels(G, pos, font_size=12, font_color="#207020")
 nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrowsize=30, width=2, alpha=1, edge_color="#00a000")
 nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G, "weight"), font_size=11, font_color="#008000")
 
-# Exécution de l'algo de Dijkstra, puis coloration des sommets parcourus + arêtes (zip)
-shortest_path = nx.dijkstra_path(G, source="Départ", target="Trésor", weight="weight")
-nx.draw_networkx_nodes(G, pos, nodelist=shortest_path, node_shape="h", node_size=2000, node_color="#0033ff", alpha=0.3)
-shortest_path_edges = list( zip(shortest_path, shortest_path[1:]) )
-nx.draw_networkx_edges(G, pos, edgelist=shortest_path_edges, edge_color="#0020a0", width=5, alpha=0.3)
+# Exécution de l'algo de Kruskal, puis coloration des sommets parcourus + arêtes (zip)
+T = nx.minimum_spanning_tree(G, weight="weight")
+nx.draw_networkx_nodes(T, pos, nodelist=T.nodes(), node_shape="h", node_size=2000, node_color="#0033ff", alpha=0.3)
+nx.draw_networkx_edges(G, pos, edgelist=T.edges(), edge_color="#0020a0", width=5, alpha=0.3)
 
 # affichage avec matplotlib
 plt.axis("off")
